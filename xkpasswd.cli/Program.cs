9f5f24581b7bf5df23fe3483c97837971d221c0e
thebhef <thebhef@gmail.com>
2021-03-22 23:17:11 -0700

    realized Spectre.Console provides version command

--- xkpasswd.cli/Program.cs
+++ xkpasswd.cli/Program.cs
@@ -25,11 +25,7 @@ namespace xkpasswd.cli
             var app = new CommandApp();
             var cts = new CancellationTokenSource();
             Console.CancelKeyPress += (_, _) => cts.Cancel();
-            app.Configure(config =>
-            {
-                config.AddCommand<GenerateCommand>("generate");
-                config.AddCommand<VersionCommand>("version");
-            });
+            app.Configure(config => { config.AddCommand<GenerateCommand>("generate"); });
             return await app.RunAsync(args.Any() ? args : new[] {"generate",}).ConfigureAwait(false);
         }
 
